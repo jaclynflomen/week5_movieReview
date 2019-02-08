3dc602682a79723396f99114944a1be3f038d603
jaclynflomen <j_floman@fanshaweonline.ca>
2019-02-08 14:20:43 -0500

    backup

--- admin/admin_createuser.php
+++ admin/admin_createuser.php
@@ -9,21 +9,11 @@ if(isset($_POST['submit'])){
     //trim would just be a starting point
     $fname = trim($_POST['fname']);
     $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
+    // $password = trim($_POST['password']);
     $email = trim($_POST['email']);
 
     $chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
     $password = substr(str_shuffle($chars), 0, 7);
-
-    function send_email (){
-        $to = $_POST['user_email'];
-        $subject = 'Your new password for Roku';
-        $message = 'Your password is: '.$_POST['user_pass'];
-        $headers = 'Message From Roku Login';
-        // $headers .= 'Reply-To: '.$_POST['user_email'];
-        mail($to, $subject, $message, $headers);
-    }
-        send_email();
 
 
     //Validation
@@ -33,6 +23,17 @@ if(isset($_POST['submit'])){
         $result = createUser($fname, $username, $password, $email);
         $message = 'Data seems alright...';
     }
+
+    function send_email (){
+        $to = 'jaclynflomen@hotmail.com';
+        $subject = 'Your new password for Roku';
+        $message = 'Your password is: '.$_POST['password'];
+        $headers = 'Message From Roku Login';
+        // $headers .= 'Reply-To: '.$_POST['email'];
+        mail($to, $subject, $message, $headers);
+        header('Location: ../admin_createuser.php');
+    }
+        send_email();
     
 }
 
